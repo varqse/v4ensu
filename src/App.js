4ddf93dbe44b5f9a1f8355990378c4ad29babf43
Ishvara Pranidhana <113882287+varqse@users.noreply.github.com>
2024-12-13 17:07:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="bg"></div>
+      <div className="MainContainer">
+        <div className="Nav">
+          <a href="#home">Home</a>
+          <a href="#works">Works</a>
+          <a href="#contacts">Contacts</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
