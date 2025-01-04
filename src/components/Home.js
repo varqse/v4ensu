2949e500e6d8c4abc12884154371e3c33e3f0e18
Ishvara Pranidhana <113882287+varqse@users.noreply.github.com>
2025-01-04 21:27:18 +0700

    Home Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,26 @@ import React from "react";
 
 const Home = () => (
   <div className="home-container">
-    <div className="myname">Ishvara Pranidhana Lakshmana</div>
-    <p className="aboutme">
-      Undergraduate Computer Science student with a strong interest in Web Development, actively seeking for any opportunities to contribute meaningfully while expanding and honing my expertise in the field.
-    </p>
+    <div className="middlecontent">
+      <div className="myname">Ishvara Pranidhana Lakshmana</div>
+      <p className="aboutme">
+        Undergraduate Computer Science student with a strong interest in Web Development, actively seeking for any opportunities to contribute meaningfully while expanding and honing my expertise in the field.
+      </p>
+    </div>
+
+    <div className="footer">
+      <div className="experience">
+        <span className="year">'25 - Now</span>
+        <span className="company">
+          <a href="https://bakorwilmalang.jatimprov.go.id/" target="_blank" rel="noopener noreferrer">
+            BAKORWIL Malang
+          </a>
+        </span>
+        <span className="position">Web Developer</span>
+      </div>
+    </div>
   </div>
 );
 
 export default Home;
+
